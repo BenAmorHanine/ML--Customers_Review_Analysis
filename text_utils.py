c74e5bc528fe9ef3bc28f21ec1b38dd11fed8bcf
hanine <haninebenamor45@gmail.com>
2025-12-04 14:28:30 +0100

    factorisation processing fct

--- text_utils.py
+++ text_utils.py
@@ -2,6 +2,7 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import nltk
+import pandas as pd
 
 nltk.download('stopwords')
 
@@ -11,10 +12,14 @@ stemmer = SnowballStemmer('french')
 
 def process_review(text):
     """Nettoyage, suppression ponctuation, stopwords, stemming"""
-    if not isinstance(text, str) or len(text.strip()) == 0:
+    if(pd.isnull(text) or not isinstance(text, str) or len(text.strip()) == 0):
         return []
+    # minuscules
     text = text.lower()
+    # suppression ponctuation et chiffres
     text = ''.join([c for c in text if c not in string.punctuation and not c.isdigit()])
+    # tokenisation et suppression stopwords
     tokens = [t for t in text.split() if t not in stop_words_fr and t not in stop_words_ar]
+    # stemming
     tokens = [stemmer.stem(t) for t in tokens]
     return tokens
